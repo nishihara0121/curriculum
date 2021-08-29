16e910e7bc6f08bfb69ff1ba7f9efede8af3683d
nishihara0121 <86067015+nishihara0121@users.noreply.github.com>
2021-08-30 02:36:55 +0900

    2章チェックテスト
    2章チェックテスト

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,20 @@
+package check;
+import constants.Constants;
+public class Check {
+	private static String firstName = "西原";
+    private static String lastName = "龍一";
+	private static String printName = firstName + lastName;
+
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Constants incon = new Constants();
+		Pet inPet = new Pet(incon.CHECK_CLASS_JAVA, incon.CHECK_CLASS_HOGE);
+		RobotPet inRobo = new RobotPet(incon.CHECK_CLASS_R2D2, incon.CHECK_CLASS_LUKE);
+		
+		System.out.println("printNameメソッド ⇨ " + printName);
+		inPet.introduce();
+		inRobo.introduce();
+	}
+
+}
