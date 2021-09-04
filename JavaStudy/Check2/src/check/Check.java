a4ad6fee47ea345dc9d2676ce54fe3503111a3b2
nishihara0121 <86067015+nishihara0121@users.noreply.github.com>
2021-09-05 00:50:25 +0900

    チェックテスト2修正1
    チェックテスト2修正1

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -3,16 +3,19 @@ import constants.Constants;
 public class Check {
 	private static String firstName = "西原";
     private static String lastName = "龍一";
-	private static String printName = firstName + lastName;
+	private static void printName(String firstName,String lastName) {
+		System.out.println("printNameメソッド ⇨ " + (firstName + lastName));
+	}
 
 	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Check check = new Check();
 		Constants incon = new Constants();
 		Pet inPet = new Pet(incon.CHECK_CLASS_JAVA, incon.CHECK_CLASS_HOGE);
 		RobotPet inRobo = new RobotPet(incon.CHECK_CLASS_R2D2, incon.CHECK_CLASS_LUKE);
 		
-		System.out.println("printNameメソッド ⇨ " + printName);
+		check.printName(firstName,lastName);
 		inPet.introduce();
 		inRobo.introduce();
 	}
